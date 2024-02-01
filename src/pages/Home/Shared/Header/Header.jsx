be2f9a6c45beb090d372049af0432d9783d40f8b
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-01 22:30:32 +0600

    moment.js added for dynamic date and time

--- src/pages/Home/Shared/Header/Header.jsx
+++ src/pages/Home/Shared/Header/Header.jsx
@@ -1,4 +1,5 @@
 import logo from '../../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
@@ -6,6 +7,7 @@ const Header = () => {
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small></p>
+                <p>{moment().format("dddd, MMMM D, YYYY,")}</p>
             </div>
         </div>
     );
