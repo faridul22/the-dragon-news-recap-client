63ab89c12fd38f3badf5912a57989b45ffd27569
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-01 22:13:00 +0600

    basic header added

--- src/pages/Home/Shared/Header/Header.jsx
+++ src/pages/Home/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
-
+import logo from '../../../../assets/logo.png'
 
 const Header = () => {
     return (
         <div>
-            <h3>This is header</h3>
+            <div className="text-center">
+                <img src={logo} alt="" />
+                <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small></p>
+            </div>
         </div>
     );
 };
