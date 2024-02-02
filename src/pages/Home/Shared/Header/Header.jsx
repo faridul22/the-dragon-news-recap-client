2d9a02f93b4694fc856518a03aa2f812f95f4e83
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-02 11:03:18 +0600

    marque and navbar added

--- src/pages/Home/Shared/Header/Header.jsx
+++ src/pages/Home/Shared/Header/Header.jsx
@@ -1,15 +1,43 @@
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../../../assets/logo.png'
 import moment from 'moment';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
-                <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small></p>
+                <p className='text-secondary fs-4'><small>Lorem ipsum dolor sit amet.</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY,")}</p>
             </div>
-        </div>
+            <div className='p-1 mb-2 bg-body-tertiary d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='fs-5 text-danger' speed={100}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">
+                                Profile
+                            </Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant='secondary'>Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
