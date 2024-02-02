b3baa753f1872fad662e1f0893214cf7cbd25164
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-02 23:29:34 +0600

    news categories data fetch from dragon news server

--- src/pages/Home/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Home/Shared/LeftNav/LeftNav.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories")
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => { console.log(error) })
+    }, [])
+
     return (
         <div>
             <h3>This is Left nav</h3>
