7279f79a6de31f47ca159bab01ac9100321655b8
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-02-02 11:40:00 +0600

    rightNav login button and social meadia added

--- src/pages/Home/Shared/RightNav/RightNav.jsx
+++ src/pages/Home/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,22 @@
-
+import { Button, ListGroup } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa6";
 
 const RightNav = () => {
     return (
         <div>
-            <h3>This is Right nav</h3>
+            <div>
+                <h3>Login with</h3>
+                <Button className="mb-2" variant="outline-primary"><FaGoogle /> Login With Google</Button>
+                <Button variant="outline-secondary"><FaGithub /> Login With GitHub</Button>
+            </div>
+            <div>
+                <h3>Find Us On</h3>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
